071c2206b55a2bcaa358232a4fa6ada7eac78b0c
bn603043 <bn603043@gmail.com>
2017-12-04 15:06:24 +0800

    hw01

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -19,7 +19,33 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
+import time
 import sys
+import httplib, urllib
+import json
+
+deviceId = "DanWu30y"
+deviceKey = "NdF0k1Zh5ZO0TdUZ" 
+def post_to_mcs(payload): 
+	headers = {"Content-type": "application/json", "deviceKey": deviceKey} 
+	not_connected = 1 
+	while (not_connected):
+		try:
+			conn = httplib.HTTPConnection("api.mediatek.com:80")
+			conn.connect() 
+			not_connected = 0 
+		except (httplib.HTTPException, socket.error) as ex: 
+			print "Error: %s" % ex 
+		        time.sleep(10)
+                        # sleep 10 seconds 
+	conn.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers) 
+	response = conn.getresponse() 
+	print( response.status, response.reason, json.dumps(payload), time.strftime("%c")) 
+	data = response.read() 
+	conn.close() 
+
+
 
 import Adafruit_DHT
 
@@ -47,8 +73,18 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 # the results will be null (because Linux can't
 # guarantee the timing of calls to read the sensor).
 # If this happens try again!
-if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-    print('Failed to get reading. Try again!')
-    sys.exit(1)
+while True:
+      h0, t0= Adafruit_DHT.read_retry(sensor, pin)
+      if humidity is not None and temperature is not None:
+            print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+
+	    payload = {"datapoints":[{"dataChnId":"humidity","values":{"value":h0}},
+		       {"dataChnId":"temperature","values":{"value":t0}}]} 
+	    post_to_mcs(payload)
+	    time.sleep(10) 
+
+      else:
+            print('Failed to get reading. Try again!')
+            sys.exit(1)
+
+
